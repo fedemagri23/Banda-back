4aa73a25e8c71080daae2c91e7588a261e52bbc8
fedemagri <fmagri@itba.edu.ar>
2025-04-10 21:02:26 -0300

    Se agrego ejecucion en network

--- src/index.js
+++ src/index.js
@@ -18,6 +18,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(routes);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
