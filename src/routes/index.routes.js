b8054533e2d0c52ffb9d577baa74dcc818d44524
fedemagri <fmagri@itba.edu.ar>
2025-04-26 19:36:41 -0300

    Se añadieron niveles de usuarios y validacion

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,14 +5,15 @@ import {
   login,
   requestPasswordChange,
   changePassword,
-  getUserById,
+  addEmployee,
+  removeEmployee,
 } from "../controllers/user.controllers.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 import { addCompany, getCompaniesFromUser } from "../controllers/company.controllers.js";
 import { getToken } from "../controllers/test.controllers.js";
 import { addSupplier, getSuppliersByCompany } from "../controllers/supplier.controllers.js";
 import { addProduct, getProductsByCompany } from "../controllers/product.controllers.js";
-import { checkCompanyOwner } from "../middleware/companyOwnerMiddleware.js";
+import { checkCompanyRole } from "../middleware/companyOwnerMiddleware.js";
 import { addPurchaseOrder, getPurchaseOrders } from "../controllers/purchase.controllers.js";
 import { addClient, getClientsByCompany, deleteClient } from "../controllers/client.controllers.js";
 import { addSaleOrder, getSaleOrders } from "../controllers/sale.controllers.js";
@@ -46,31 +47,33 @@ router.post("/user/login", login);
 router.post("/user/request-password-change", requestPasswordChange);
 router.post("/user/change-password", changePassword);
 router.get("/user/get-all", getUsers);
-router.get("/user/:id", getUserById);
+router.post("/employee/post/:companyId", verifyToken, checkCompanyRole(1), addEmployee);
+router.delete("/employee/delete/:companyId", verifyToken, checkCompanyRole(1), removeEmployee);
+
 
 router.post("/company/post", verifyToken, addCompany);
 router.get("/company/get-all", verifyToken, getCompaniesFromUser);
 
-router.post("/supplier/post/:companyId", verifyToken, checkCompanyOwner, addSupplier);
-router.get("/supplier/get-all/:companyId", verifyToken, checkCompanyOwner, getSuppliersByCompany);
-router.delete("/supplier/delete/:companyId/:supplierId", verifyToken, checkCompanyOwner, deleteSupplier);
+router.post("/supplier/post/:companyId", verifyToken, checkCompanyRole(2), addSupplier);
+router.get("/supplier/get-all/:companyId", verifyToken, checkCompanyRole(2), getSuppliersByCompany);
+router.delete("/supplier/delete/:companyId/:supplierId", verifyToken, checkCompanyRole(2), deleteSupplier);
 
-router.post("/product/post/:companyId", verifyToken, checkCompanyOwner, addProduct);
-router.get("/product/get-all/:companyId", verifyToken, checkCompanyOwner, getProductsByCompany);
+router.post("/product/post/:companyId", verifyToken, checkCompanyRole(2), addProduct);
+router.get("/product/get-all/:companyId", verifyToken, checkCompanyRole(2), getProductsByCompany);
 
-router.post("/purchase/post/:companyId", verifyToken, checkCompanyOwner, addPurchaseOrder);
-router.get("/purchase/get-all/:companyId", verifyToken, checkCompanyOwner, getPurchaseOrders);
+router.post("/purchase/post/:companyId", verifyToken, checkCompanyRole(2), addPurchaseOrder);
+router.get("/purchase/get-all/:companyId", verifyToken, checkCompanyRole(2), getPurchaseOrders);
 
-router.post("/client/post/:companyId", verifyToken, checkCompanyOwner, addClient);
-router.get("/client/get-all/:companyId", verifyToken, checkCompanyOwner, getClientsByCompany);
-router.delete("/client/delete/:companyId/:clientId", verifyToken, checkCompanyOwner, deleteClient);
+router.post("/client/post/:companyId", verifyToken, checkCompanyRole(2), addClient);
+router.get("/client/get-all/:companyId", verifyToken, checkCompanyRole(2), getClientsByCompany);
+router.delete("/client/delete/:companyId/:clientId", verifyToken, checkCompanyRole(2), deleteClient);
 
-router.post("/sale/post/:companyId", verifyToken, checkCompanyOwner, addSaleOrder);
-router.get("/sale/get-all/:companyId", verifyToken, checkCompanyOwner, getSaleOrders);
+router.post("/sale/post/:companyId", verifyToken, checkCompanyRole(2), addSaleOrder);
+router.get("/sale/get-all/:companyId", verifyToken, checkCompanyRole(2), getSaleOrders);
 
-router.get("/inventory/get-all/:companyId", verifyToken, checkCompanyOwner, getInventoryByCompany); 
+router.get("/inventory/get-all/:companyId", verifyToken, checkCompanyRole(2), getInventoryByCompany); 
 
-router.get("/metric/order/balance-chart/:companyId", verifyToken, checkCompanyOwner, getOrderBalanceChart); 
+router.get("/metric/order/balance-chart/:companyId", verifyToken, checkCompanyRole(2), getOrderBalanceChart); 
 
 
 // TODO: Al final BORRAR estos controllers
